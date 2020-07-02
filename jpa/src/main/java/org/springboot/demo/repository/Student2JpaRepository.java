61434f953dc034101693f82e9ed9b1afecd9a8e4
Meng <sunmeng90@126.com>
2020-07-02 20:50:43 +0800

    use mysql 8

--- jpa/src/main/java/org/springboot/demo/repository/Student2JpaRepository.java
+++ jpa/src/main/java/org/springboot/demo/repository/Student2JpaRepository.java
@@ -10,6 +10,12 @@ import java.util.List;
 public interface Student2JpaRepository extends JpaBaseRepository<Student, Integer> {
 
 
-    @Query("select stu from Student stu join fetch stu.courses cs join fetch cs.course c left join fetch cs.books b where stu.audit.effectiveEndDate = :" + YEAR_9999 + " and cs.audit.effectiveEndDate = :" + YEAR_9999 + " and c.audit.effectiveEndDate = :" + YEAR_9999 + " and b.audit.effectiveEndDate = :" + YEAR_9999)
+    @Query("select distinct stu from Student stu " +
+            "join fetch stu.courses cs join fetch cs.course c " +
+            "left join fetch cs.books b " +
+            "where stu.audit.effectiveEndDate = :" + YEAR_9999 + " " +
+            "and cs.audit.effectiveEndDate = :" + YEAR_9999 + " " +
+            "and c.audit.effectiveEndDate = :" + YEAR_9999 + " " +
+            "and b.audit.effectiveEndDate = :" + YEAR_9999)
     List<Student> findAllStudentByJoin();
 }
